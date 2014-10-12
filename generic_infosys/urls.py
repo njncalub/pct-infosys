9e2805ccda3f5658a0a3fb6b66ecdbe152170edb
Nap Joseph N. Calub <njncalub@gmail.com>
2014-10-12 20:32:50 +0800

    updated index page

--- generic_infosys/urls.py
+++ generic_infosys/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from student_profiling.views import IndexView
+
+
 admin.site.site_header = 'PCT Information System'
 admin.site.site_title  = 'PCT Information System'
 admin.site.index_title = 'Dashboard'
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', IndexView.as_view()),
 )
--- student_profiling/views.py
+++ student_profiling/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+class IndexView(View):
+    template_name = "student_profiling/index.html"
+    context = {}
+    title = "Home"
+    context['title'] = title
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.context)
